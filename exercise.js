5baa9edfa30c720142a8abe06da9d837ab7196d0
May Chen <maychen@Mays-MacBook-Pro.local>
2018-02-16 00:37:21 -1000

    completed ex3

--- exercise.js
+++ exercise.js
@@ -66,7 +66,12 @@ valuesDonutBox(donutBox);
 /*@param {Object}
 @return {Object}*/
 
+function removeTaste(obj){
+	delete obj.taste;
+	return obj;
+}
 
+console.log(removeTaste(donutBox));
 
 /*4. Create a function that will take in an object and will return all the values of the object in an array.*/
 
